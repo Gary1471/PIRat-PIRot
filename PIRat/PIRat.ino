878c17c76e0ffcae856ca606a45ed06980a162f8
gary <129380209+Gary1471@users.noreply.github.com>
2025-03-17 17:36:05 +0100

    Cleanup

--- PIRat/PIRat.ino
+++ PIRat/PIRat.ino
@@ -6,22 +6,21 @@
 #define BUTTON_PIN 5        // GPIO 5 = D1 na desce
 #define LED_PIN 0           // GPIO 0 = D3 na desce
 
-volatile bool buttonPressed = false;  // Flag pro ISR
-int mode = 0;                         // Režim zařízení: 0 = LED, 1 = LED + BUZZER, 2 = NONE
-bool motionDetected = false;          // Stav pohybu
+volatile bool buttonPressed = false;  
+int mode = 0;                         // Režim zařízení: 0 = LED, 1 = LED + BUZZER, 2 = TICHÝ
+bool motionDetected = false;          
 
-// Struktura pro odesílání dat
 typedef struct struct_message {
   int movementDetected;  
 } struct_message;
 
 struct_message myData;
 
-// Adresa PIRot (nahraďte skutečnou MAC adresou PIRot)
+// Adresa PIRot
 uint8_t broadcastAddress[] = {0xC8, 0xC9, 0xA3, 0x2F, 0x7E, 0xA9};
 
 void ICACHE_RAM_ATTR onButtonPress() {
-  buttonPressed = true; // Nastaví flag (bezpečné pro ISR)
+  buttonPressed = true;
 }
 
 void setup() {
@@ -42,12 +41,12 @@ void setup() {
   }
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
 
-  // Předpokládáme, že PIRot je vždy dostupný, připojujeme se k němu.
+  
   esp_now_add_peer(broadcastAddress, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
 }
 
 void loop() {
-  // Zpracování stisku tlačítka mimo ISR
+  
   if (buttonPressed) {
     mode = (mode + 1) % 3; // Přepnutí režimu (0, 1, 2)
     Serial.print("Režim změněn na: ");
@@ -59,7 +58,6 @@ void loop() {
       Serial.println("NONE režim");
     }
 
-    // Potvrzení bliknutím LED
     digitalWrite(LED_PIN, HIGH);
     delay(100);
     digitalWrite(LED_PIN, LOW);
@@ -74,7 +72,6 @@ void loop() {
     esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
     Serial.println("Zpráva o pohybu odeslána PIRot");
 
-    // Podle režimu provést akce
     if (mode == 0) {  // LED režim
       Serial.println("LED režim: LED bliká");
       digitalWrite(LED_PIN, HIGH);
@@ -87,16 +84,15 @@ void loop() {
       delay(500);
       digitalWrite(LED_PIN, LOW);
       digitalWrite(BUZZER_PIN, LOW);
-    } else {  // NONE režim
-      // V režimu NONE pouze odesíláme zprávu, žádná vizuální nebo zvuková indikace
-      Serial.println("NONE režim: Pouze zpráva odeslána.");
+    } else {  // TICHÝ režim
+      Serial.println("TICHÝ režim: Pouze zpráva odeslána.");
     }
   } else if (pirState == LOW && motionDetected) {
     motionDetected = false;
     myData.movementDetected = 0;
     esp_now_send(broadcastAddress, (uint8_t *)&myData, sizeof(myData));
-    Serial.println("Zpráva o pohybu přestal odeslána PIRot");
+    Serial.println("Zpráva odeslána PIRotu"); // Konec pohybu
   }
 
-  delay(100);  // Krátká pauza pro stabilitu
+  delay(100);  // delay pro stabilitu
 }
